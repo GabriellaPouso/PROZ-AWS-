29820d2f87efb50d63be5803f70f8b66ef28d747
GabriellaPouso <pouso.gabi@gmail.com>
2024-09-21 13:48:00 -0300

    renamed:    AndaresDoPredio -> AndaresDoPredio.py renamed:    "CategoriaHabilita\303\247\303\243o" -> CategoriaHabilitacao.py new file:   ConvidadoseFeriados.py renamed:    "La\303\247oRepeti\303\247\303\243o" -> LacoDeRepeticao.py renamed:    Projeto idade -> ProjetoIdade.py deleted:    TrueOrFalseactivity renamed:    exemplos -> exemplos.py

---
+++ ConvidadoseFeriados.py
@@ -0,0 +1,31 @@
+nota=10         #atividade 1
+print(input(nota<=10))          #True
+
+
+
+nota=6         #atividade 2
+faltas=4
+print(input(nota<=6 and faltas<=3))         #False
+
+
+
+convidados=3         #atividade 3
+fumante=False
+print(input(convidados>4 or fumante==True))        #False
+
+
+
+dia="qua"         #atividade 4
+print(input(dia=="sab" or dia=="dom"))        #False
+
+
+
+feriado=True         #atividade 5
+print(input(not(feriado==False)))          #True
+
+
+
+dia="ter"         #atividade 6
+feriado=False
+print(input(dia=="seg" or not(feriado==False)))        #False
+
