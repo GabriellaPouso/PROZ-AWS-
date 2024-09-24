a4efcbe83fdf91da8af86e277b0193931fbef158
GabriellaPouso <pouso.gabi@gmail.com>
2024-09-23 22:44:24 -0300

    change

--- Aula05.py
+++ Aula05.py
@@ -92,3 +92,15 @@ def lista():
         nlista.append(valor)
 list = lista()
 print(list)
+
+def _list(lista):
+    print (lista [0:])
+
+list = [9,'2',3,4,5,90]
+_list(list)
+
+list1 = [98,24,3,0,12,70]
+_list(list1)
+
+list2 = ['Feijão','Arroz','Farinha','carne','frango','banana']
+_list(list2)
