f75d53a1139fc39883afb2512c769e456c172b91
GabriellaPouso <pouso.gabi@gmail.com>
2024-09-23 22:15:02 -0300

    changes

--- Aula05.py
+++ Aula05.py
@@ -1,4 +1,80 @@
-with open("","r") as arquivo:
+idade=int(input("Digite a sua idade: "))
+if idade>=18:
+    print("Maior de idade")
+else:
+    idade<18
+    print("Menor de idade")
+
+i=0
+while(i<=10):
+    if(i % 2 ==1):
+        print(f"É impar {i}")
+    if(i % 2 ==0):
+        print(f"É par {i}")
+    i=i+1
+
+i=0
+while(i<=10):
+    if(i % 2 ==1):
+        print(f"É impar {i}")
+    if(i % 2 ==0):
+        print(f"É par {i}")
+    i=i+1
+
+i = 1       #dá pra colocar um i=int(input("Digite o número: "))
+a = 2       #dá pra colocar um a=int(input("Digite o número: "))
+print("Adição")
+while(i <= 10):
+    print(f"{a} + {i} = {a+i}")
+    i = i + 1
+print("Multiplicação")
+i = 1
+while(i <= 10):
+    print(f"{a} * {i} = {a*i}")
+    i = i + 1
+   
+i = 1
+op = 's'
+while (op == 's'):
+    nome = input("Nome: ")
+    print("O nome informado foi: ", nome)
+    op = input("Deseja Continuar(s|n)?: ")
+    i = i+1
+    
+a=2
+b="2"
+print(a == b) #compara type
+
+for i in range(0,11):
+    print(i)
+    
+lista = ["Gabriella", "Thiago", "Anna Luíza"]
+for i in range(3):
+    print(i, lista)
+    
+for i in "Gabriella":
+    print(i)
+   
+produtos = {
+  "faca": 10,
+  "garfo": 10,
+  "panela": 200,
+  "frigideira": 50,
+  "flavorstone": 300,
+}
+for lista in produtos:
+  print(lista, "R$",produtos[lista])
+ 
+def soma(nomes, numeros):
+    print(f"Lista de nomes: {nomes}")
+    print(f"Vetor de números: {numeros}")
+ 
+lista_nomes = ["Alice", "Bob", "Carlos", "Diana", "Eva"]
+vetor_numeros = [1, 2, 3, 4, 5]
+soma(lista_nomes, vetor_numeros)
+
+soma = 0
+with open("dados/soma.txt","r") as arquivo:
     texto = arquivo.read()
 listaTexto = (texto.split())
 print(listaTexto)
@@ -7,3 +83,12 @@ for lista in listaTexto:
     soma = soma + valor
 print(soma)
 
+def lista():
+    nlista= []
+    while(True):
+        valor=input("Informe um valor (sair = s): ")
+        if(valor == "s"):
+            return nlista
+        nlista.append(valor)
+list = lista()
+print(list)
--- exemplos.py
+++ exemplos.py
@@ -1,29 +1,3 @@
-idade=int(input("Digite a sua idade: "))
-if idade>=18:
-    print("Maior de idade")
-else:
-    idade<18
-    print("Menor de idade")
-
-idade=int(input("Digite a sua idade: "))
-result = "Maior de idade" if idade>=18 and "Menor de idade" else idade<18
-
-i=0
-while(i<=10):
-    if(i % 2 ==1):
-        print(f"É impar {i}")
-    if(i % 2 ==0):
-        print(f"É par {i}")
-    i=i+1
-
-i=0
-while(i<=10):
-    if(i % 2 ==1):
-        print(f"É impar {i}")
-    if(i % 2 ==0):
-        print(f"É par {i}")
-    i=i+1
-
 i = 1
 a = 2
 print("Adição")
@@ -35,7 +9,6 @@ i = 1
 while(i <= 10):
     print(f"{a} * {i} = {a*i}")
     i = i + 1
-    
     i = 1
 op = 's'
 while (op == 's'):
@@ -44,36 +17,3 @@ while (op == 's'):
     op = input("Deseja Continuar: ")
     i = i+1
     
-a=2
-b="2"
-print(a == b) #compara type
-
-for i in range(0,11):
-    print(i)
-    
-lista = ["Gabriella", "Thiago", "Anna Luíza"]
-for i in range(3):
-    print(i, lista)
-    
-for i in "Gabriella":
-    print(i)
-    
-produtos = {
-  "faca": 10,
-  "garfo": 10,
-  "panela": 200,
-  "frigideira": 50,
-  "flavorstone": 300,
-}
-for lista in produtos:
-  print(lista, produtos[lista])
-  
-def soma(nomes, numeros):
- 
-    print(f"Lista de nomes: {nomes}")
-    print(f"Vetor de números: {numeros}")
- 
-lista_nomes = ["Alice", "Bob", "Carlos", "Diana", "Eva"]
-vetor_numeros = [1, 2, 3, 4, 5]
- 
-soma(lista_nomes, vetor_numeros)
