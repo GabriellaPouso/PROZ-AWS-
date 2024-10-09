38a62940e1864cf1156a483d6fdbe6eadca15317
Gabriella Cecília Pereira Pouso Veiga <pouso.gabi@gmail.com>
2024-10-09 21:23:57 +0000

    new file:   OpComp.py

---
+++ OpComp.py
@@ -0,0 +1,16 @@
+num1 = input("Digite um número: ")
+num1 = int(num1)
+num2 = input("Digite outro número: ")
+num2 = int(num2)
+if(num1==num2): #operador de IGUALDADE
+    print("O número %d é igual a %d."%(num1,num2))
+if(num1!=num2): #operador de DIFERENÇA
+    print("O número %d é diferente de %d"%(num1,num2))
+if(num1<num2): #operador MENOR QUE
+    print("O número %d é menor que %d"%(num1,num2))
+if(num1>num2): #operador MAIOR QUE
+    print("O número %d é maior que %d"%(num1,num2))
+if(num1<=num2): #operador MENOR OU IGUAL QUE
+    print("O número %d é maior ou igual a %d"%(num1,num2))
+if(num1>=num2): #operador MAIOR OU IGUAL QUE
+    print("O número %d é maior ou igual a %d"%(num1,num2))
