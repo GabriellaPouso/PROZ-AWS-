4a6063ccddcb5462bdd9acf01e941e3413c3e55d
GabriellaPouso <pouso.gabi@gmail.com>
2024-10-11 14:00:03 -0300

    atividade.lojacosmetic

--- aulacssehatml.py
+++ aulacssehatml.py
@@ -33,3 +33,29 @@ print(lista_musicos[2],lista_musicos[9],lista_musicos[14])
 print(lista_musicos[2])
 print(lista_musicos[9])
 print(lista_musicos[14])
+
+lista_frutas = ['maçã', 'banana', 'pera']
+print(lista_frutas[0])
+print(lista_frutas[1])
+print(lista_frutas[2])
+# Imprimirá:
+# maçã
+# banana
+# pera
+
+lista_frutas = ['maçã', 'banana', 'pera']
+for i in range(3):
+    print(lista_frutas[i])
+# Imprimirá:
+# maçã
+# banana
+# pera
+
+lista_num = [2, 45, 65, 78, 126, 987, 457, 345, 679, 107, 2345, 452, 3, 34, 560]
+for i in range(15):
+    print(lista_num[i])
+    
+lista_num = [2, 45, 65, 78, 126, 987, 457, 345, 679, 107, 2345, 452, 3, 34, 560]
+for i in range(len(lista_num)):
+    print(lista_num[i])
+
