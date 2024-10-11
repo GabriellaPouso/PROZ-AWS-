27a6edea423e52f3f01a988bc5baa71cd54da3f2
GabriellaPouso <pouso.gabi@gmail.com>
2024-10-11 13:09:26 -0300

    array_aula

---
+++ arra.py
@@ -0,0 +1,27 @@
+operacao=(5+3)*(9-4)
+print(operacao)
+# a variável 'operacao' tem um dado só, o inteiro 40
+string="Eu "+"gosto "+"de "+"panquecas"
+print(string)
+# a variável 'string' tem um único dado, a string "Eu gosto de panquecas"
+
+lista_numeros=[3,19,54,-2]
+print(lista_numeros)
+lista_floats=[0.3,3.9,89.15,123.45]
+print(lista_floats)
+lista_strings=["Eu","gosto","de","panquecas"]
+print(lista_strings)
+lista_booleanos=[False,True,True,False]
+print(lista_booleanos)
+lista_listas=[[1,2,3],['a','b','c']]
+print(lista_listas)
+lista_mista=[10,'dez',True]
+print(lista_mista)
+
+#Atividade DESENVOLVIMENTO EXTRA 1.1
+
+lista_produtos=['tomate','cebola','cenoura','alface','batata']
+print(lista_produtos)
+
+lista_anos = [1960,1969,1990,1991,1993]
+print(lista_anos)
